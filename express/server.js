49d4937855ae0836ad73be06e1963e2a10e9bc54
Eduardo E. Punto Jr <58497534+mepuntojr1@users.noreply.github.com>
2022-12-25 22:21:32 +0800

    Update server.js

--- express/server.js
+++ express/server.js
@@ -1,36 +1,58 @@
-'use strict';
-const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
-const app = express();
-const bodyParser = require('body-parser');
-const router = express.Router();
+const http = require('http');
+// or use import http from 'http';
 
-// Link to views folder.
-let views = path.join(__dirname, '../');
 
-// Home route.
-router.get('/', (req, res) => {
-  res.sendFile('index.html', { root: views });
+/* your app config here */
+
+app.post('/api/BLABLA', (oreq, ores) => {
+  const options = {
+    // host to forward to
+    host: 'www.google.com',
+    // port to forward to
+    port: 80,
+    // path to forward to
+    path: '/api/BLABLA',
+    // request method
+    method: 'POST',
+    // headers to send
+    headers: oreq.headers,
+  };
+
+  const creq = http
+    .request(options, pres => {
+      // set encoding
+      pres.setEncoding('utf8');
+
+      // set http status code based on proxied response
+      ores.writeHead(pres.statusCode);
+
+      // wait for data
+      pres.on('data', chunk => {
+        ores.write(chunk);
+      });
+
+      pres.on('close', () => {
+        // closed, let's end client request as well
+        ores.end();
+      });
+
+      pres.on('end', () => {
+        // finished, let's finish client request as well
+        ores.end();
+      });
+    })
+    .on('error', e => {
+      // we got an error
+      console.log(e.message);
+      try {
+        // attempt to set error message and http status
+        ores.writeHead(500);
+        ores.write(e.message);
+      } catch (e) {
+        // ignore
+      }
+      ores.end();
+    });
+
+  creq.end();
 });
-
-// Other routes.
-router.get('/page1', function(req, res){
-  res.sendFile('page1.html', { root: views });
-});
-router.get('/page2', function(req, res){
-  res.sendFile('page2.html', { root: views });
-});
-router.get('/page3', function(req, res){
-  res.sendFile('page3.html', { root: views });
-});
-router.get('/page4', function(req, res){
-  res.sendFile('page4.html', { root: views });
-});
-
-
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda (express/server.js)
-
-module.exports = app;
-module.exports.handler = serverless(app);
